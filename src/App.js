8f6adff933e47459c8c43445ef92f17da7093a2c
nach66 <41806538+nach66@users.noreply.github.com>
2020-07-13 01:26:49 +0300

    update 97

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
     <NavBar/>
       <Switch>
         <Route exact path="/" component={Home}/>
-        <Route path="/rooms" component={Rooms}/>
-        <Route path="/room/:slug" component={SingleRoom}/>
-        <Route path="/error" component={Error}/>
+        <Route exact path="/rooms" component={Rooms}/>
+        <Route exact path="/rooms/:slug" component={SingleRoom}/>
+        <Route component={Error}/>
       </Switch>
     </>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import Services from '../components/Servies';
+import FeaturedRoom from '../components/FeaturedRoom';
 import {Link} from 'react-router-dom';
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
                 </Banner>
             </Hero>
             <Services/>
+            <FeaturedRoom/>
         </>
     );
 }
